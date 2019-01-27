9e7c6f91ede5b3210ed023b6719dd6e2ef75637c
Thierry Rascle <thierr26@free.fr>
2019-01-27 14:17:55 +0100

    Make a with clause a private with clause

--- src/apsepp-scope_bound_locks.ads
+++ src/apsepp-scope_bound_locks.ads
@@ -1,7 +1,7 @@
 -- Copyright (C) 2019 Thierry Rascle <thierr26@free.fr>
 -- MIT license. Please refer to the LICENSE file.
 
-with Ada.Finalization;
+private with Ada.Finalization;
 
 package Apsepp.Scope_Bound_Locks is
 
