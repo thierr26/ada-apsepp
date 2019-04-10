9c177d7db9dc8b23bfaf8554b365a2e116454fe2
Thierry Rascle <thierr26@free.fr>
2019-04-10 22:00:47 +0200

    Remove some creational functions

--- src/apsepp-test_node_class-runner_sequential-create.adb
+++ src/apsepp-test_node_class-runner_sequential-create.adb
@@ -1,17 +1,17 @@
 -- Copyright (C) 2019 Thierry Rascle <thierr26@free.fr>
 -- MIT license. Please refer to the LICENSE file.
 
-with Apsepp.Test_Node_Class.Suite_Stub.Create;
+with Apsepp.Test_Node_Class.Suite_Stub;
 
 function Apsepp.Test_Node_Class.Runner_Sequential.Create
   (Root_Node_Access              : Test_Node_Access;
-   Test_Reporter_Instance_Access : Test_Reporter_Access                := null;
+   Test_Reporter_Instance_Access : Test_Reporter_Access         := null;
    Reporter_Access_Set_CB        : Scope_Bound_Locks.SB_Lock_CB := null)
   return Test_Runner_Sequential is
 
 begin
 
-   return (Suite_Stub.Create
+   return (Test_Suite_Stub
              with Child_Access    => Root_Node_Access,
                   Reporter_Access => Test_Reporter_Instance_Access,
                   R_A_S_CB        => Reporter_Access_Set_CB);
