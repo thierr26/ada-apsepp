add15f860c2521355ad6453606292eb2d6574c94
Thierry Rascle <thierr26@free.fr>
2019-06-30 16:25:38 +0200

    Add (and comment out) exception tracing
    GNAT.Exception_Traces is used.

--- src/_test/harness/apsepp_test.adb
+++ src/_test/harness/apsepp_test.adb
@@ -1,12 +1,19 @@
 -- Copyright (C) 2019 Thierry Rascle <thierr26@free.fr>
 -- MIT license. Please refer to the LICENSE file.
 
+-- with GNAT.Exception_Traces; -- For debugging.
 with Apsepp_Test_Harness;
 
 procedure Apsepp_Test is
 
+   -- use GNAT.Exception_Traces; -- For debugging.
+
 begin
 
+   -- Trace_On (Every_Raise); -- For debugging.
+
    Apsepp_Test_Harness.Apsepp_Test_Procedure;
+
+   -- Trace_Off; -- For debugging.
 
 end Apsepp_Test;
