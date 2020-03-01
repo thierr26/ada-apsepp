0a1312d2e0de4db7990088bb5e24a0770ac8cc50
Thierry Rascle <thierr26@free.fr>
2020-03-01 21:29:40 +0100

    Reintroduce exit status in Apsepp_Test_Harness
    And do not print anymore to standard output.

--- src/_test/harness/apsepp_test_harness.adb
+++ src/_test/harness/apsepp_test_harness.adb
@@ -1,8 +1,7 @@
 -- Copyright (C) 2019-2020 Thierry Rascle <thierr26@free.fr>
 -- MIT license. For more information, please refer to the LICENSE file.
 
-with Apsepp.Output,
-     Apsepp.Generic_Shared_Instance.Finalized_S_R_Dealloc,
+with Ada.Command_Line,
      Apsepp.Test_Node_Class.Runner_Sequential.Create;
 
 package body Apsepp_Test_Harness is
@@ -11,36 +10,23 @@ package body Apsepp_Test_Harness is
 
    procedure Apsepp_Test_Procedure is
 
-      use Apsepp.Output,
-          Apsepp.Test_Node_Class,
+      use Ada.Command_Line,
           Apsepp.Test_Node_Class.Runner_Sequential;
 
-      Output_Lock_Holder : Output_Shared_Instance.Holder;
-
-      Output_Instance_Access : constant Output_Standard_Access
-        := (if Output_Lock_Holder.Holds then
-               new Output_Standard
-            else
-               null);
-
-      package Output_S_R is new Output_Shared_Instance.Finalized_S_R_Dealloc
-        (Instance_Access      => Output_Instance_Access,
-         Lock_Holder_Type     => Output_Shared_Instance.Holder,
-         Lock_Holder_Instance => Output_Lock_Holder);
+      use all type Apsepp.Test_Node_Class.Test_Outcome;
 
       Test_Runner : Test_Runner_Sequential := Create (Test_Suite'Access);
 
-      Outcome : Test_Outcome;
-
-      pragma Unreferenced (Output_S_R);
+      Outcome : Apsepp.Test_Node_Class.Test_Outcome;
 
    begin
 
-      Output.Put_Line ("Hello world!");
-
       Test_Runner.Early_Run;
       Test_Runner.Run (Outcome);
-      Output.Put_Line (Test_Outcome'Image (Outcome));
+      Set_Exit_Status (if Outcome = Passed then
+                          Success
+                       else
+                          Failure);
 
    end Apsepp_Test_Procedure;
 
--- src/_test/harness/apsepp_test_harness.ads
+++ src/_test/harness/apsepp_test_harness.ads
@@ -1,8 +1,7 @@
 -- Copyright (C) 2019-2020 Thierry Rascle <thierr26@free.fr>
 -- MIT license. For more information, please refer to the LICENSE file.
 
-private with Apsepp_Test_Suite,
-             Apsepp.Output_Class.Standard;
+private with Apsepp_Test_Suite;
 
 package Apsepp_Test_Harness is
 
@@ -10,11 +9,8 @@ package Apsepp_Test_Harness is
 
 private
 
-   use Apsepp_Test_Suite,
-       Apsepp.Output_Class.Standard;
+   -- use Apsepp_Test_Suite;
 
-   type Output_Standard_Access is access Output_Standard;
-
-   Test_Suite : aliased Apsepp_T_S;
+   Test_Suite : aliased Apsepp_Test_Suite.Apsepp_T_S;
 
 end Apsepp_Test_Harness;
