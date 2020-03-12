1501e6044b0601f4ddedbb88a908b0d79986171d
Thierry Rascle <thierr26@free.fr>
2020-03-12 21:24:20 +0100

    Reintroduce (commented out) debug specific lines

--- src/_test/harness/apsepp_test.adb
+++ src/_test/harness/apsepp_test.adb
@@ -1,12 +1,20 @@
 -- Copyright (C) 2019-2020 Thierry Rascle <thierr26@free.fr>
 -- MIT license. For more information, please refer to the LICENSE file.
 
+-- with GNAT.Exception_Traces; -- For debugging.
+
 with Apsepp_Test_Harness;
 
 procedure Apsepp_Test is
 
+   -- use GNAT.Exception_Traces; -- For debugging.
+
 begin
 
+   -- Trace_On (Every_Raise); -- For debugging.
+
    Apsepp_Test_Harness.Apsepp_Test_Procedure;
+
+   -- Trace_Off; -- For debugging.
 
 end Apsepp_Test;
