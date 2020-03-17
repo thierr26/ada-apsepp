379b8e2b569fe51433a520de369b755b8e4804de
Thierry Rascle <thierr26@free.fr>
2020-03-18 00:00:21 +0100

    Update copyright notices

--- src/apsepp-output_class-standard.ads
+++ src/apsepp-output_class-standard.ads
@@ -1,4 +1,4 @@
--- Copyright (C) 2019 Thierry Rascle <thierr26@free.fr>
+-- Copyright (C) 2019-2020 Thierry Rascle <thierr26@free.fr>
 -- MIT license. For more information, please refer to the LICENSE file.
 
 package Apsepp.Output_Class.Standard is
