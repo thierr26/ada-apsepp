76d520b487d654a742ec8d21ece42a42d58430bb
Thierry Rascle <thierr26@free.fr>
2020-04-11 22:01:03 +0200

    Rework 'Single_Line_From_String'

--- src/apsepp-text_class-r-single_line_from_string.adb
+++ src/apsepp-text_class-r-single_line_from_string.adb
@@ -6,14 +6,27 @@ with Apsepp.Text_Class.R.Create_Single_Line;
 function Apsepp.Text_Class.R.Single_Line_From_String
   (Line : String) return RO_Text_Single_Line is
 
-   -- Sliding makes the conversion to 'Character_Array' below safer. (For the
-   -- case where 'Positive'Pos (Line'Last)
-   --               >
-   --             Character_Index'Pos (Character_Index'Last'.)
-   Slided_Line : constant String (1 .. Line'Length) := Line;
-
 begin
 
-   return Create_Single_Line (Character_Array (Slided_Line));
+   if Positive'Pos (Line'Last)
+        >
+      Character_Index'Pos (Character_Index'Last) then
+      -- A conversion of 'Line' to type 'Character_Array' would raise
+      -- 'Constraint_Error'. Sliding solves the issue. We know, thanks to the
+      -- pre-condition, that type 'Character_Array' can accomodate 'Line'.
+
+      declare
+         Slided_Line : constant String (1 .. Line'Length) := Line;
+      begin
+         return Create_Single_Line (Character_Array (Slided_Line));
+                                                               -- Early return.
+      end;
+
+   end if;
+
+   -- We get there only if the conversion of 'Line' to type 'Character_Array'
+   -- is safe.
+
+   return Create_Single_Line (Character_Array (Line));
 
 end Apsepp.Text_Class.R.Single_Line_From_String;
